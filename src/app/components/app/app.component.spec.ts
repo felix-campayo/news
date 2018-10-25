518f21562d3cff97edce7e960ddf7b6da6e35053
Félix <campayo@gmail.com>
2018-10-25 19:31:06 +0200

    Routing test

--- src/app/components/app/app.component.spec.ts
+++ src/app/components/app/app.component.spec.ts
@@ -1,35 +1,58 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { ArchivesModule } from './../../modules/archives/archives.module';
+import { Location } from '@angular/common';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { RouterTestingModule, SpyNgModuleFactoryLoader } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { AppComponent } from '..';
+import { ROUTES } from 'src/app/modules/app-routing.module';
+import { NewsModule } from 'src/app/modules/news/news.module';
+import { NgModuleFactoryLoader, NgZone } from '@angular/core';
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
+describe('Router: App', () => {
+  let location: Location;
+  let router: Router;
+  let fixture;
+  let loader: SpyNgModuleFactoryLoader;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule.withRoutes(ROUTES),
+        NewsModule,
+        ArchivesModule
       ],
       declarations: [
         AppComponent
-      ],
-    }).compileComponents();
+      ]
+    });
+
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
+    fixture = TestBed.createComponent(AppComponent);
+    router.initialNavigation();
+    fixture.detectChanges();
+    loader = TestBed.get(NgModuleFactoryLoader);
+  });
+
+  it('navigate to "news" redirects you to /news', fakeAsync(() => {
+    loader.stubbedModules = { lazyModule: NewsModule };
+    router.resetConfig([
+      { path: 'news', loadChildren: 'lazyModule' },
+    ]);
+
+    router.navigate(['/news']);
+    tick();
+    expect(location.path()).toBe('/news');
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  it('navigate to "archives" takes you to /archives', fakeAsync(() => {
+    loader.stubbedModules = { lazyModule: ArchivesModule };
+    router.resetConfig([
+      { path: 'archives', loadChildren: 'lazyModule' },
+    ]);
 
-  it(`should have as title 'news'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('news');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to news!');
-  });
+    router.navigate(['/archives']);
+    tick();
+    expect(location.path()).toBe('/archives');
+  }));
 });
