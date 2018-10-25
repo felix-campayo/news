95c4e6c6bd0887faa6f7b1f3b6760ad260d353c5
Félix <campayo@gmail.com>
2018-10-25 20:52:27 +0200

    Utils done

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { FilterByDatePipe } from './pipes';
+import { MessageUtil } from './utils';
 
 @NgModule({
     declarations: [
         FilterByDatePipe
+    ],
+    providers: [
+        MessageUtil
     ],
     exports: [
         FilterByDatePipe
